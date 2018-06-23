f423f4bae8aed79360dd3d053748ee4b371608b1
umi <0123mark3210@gmail.com>
2018-06-23 19:19:15 +0900

    docomo雑談APIに変更

--- js/chat.js
+++ js/chat.js
@@ -1,104 +1,148 @@
-// recruit社の提供しているTALK APIを使用 
-// 2018/06/22
+// docomo自然対話:雑談会話APIを利用
+// 2018・06・23 reference:https://dev.smt.docomo.ne.jp/?p=docs.api.page&api_name=natural_dialogue&p_name=api_4
+// codeの参考:https://teratail.com/questions/132322 
 
-$(document).ready(function(){
-    // 導入の会話を生成
+var ampm = function(hour){
+    var result;
+    if(parseInt(hour,10) < 12){
+        result = "午前";
+    }else{
+        result = "午後";
+    }
+    return result;
+}
+
+var addChatList = function(id, message){
     // liをchat_listに追加
     var list = document.createElement("li");
     var tag_list = document.getElementById("chat_list").appendChild(list);
-    // liに.botを追加
-    tag_list.classList.add("bot");
+    tag_list.classList.add(id);
 
-    // アイコンフォントの追加
+    var profile = document.getElementsByClassName(id);
+
+    // webフォント・nameの追加
     var tag_icon = document.createElement("i");
     tag_icon.classList.add("fas", "fa-user-circle");
-    var bot_profile = document.getElementsByClassName("bot");
-    bot_profile[bot_profile.length-1].appendChild(tag_icon);
+    profile[profile.length-1].appendChild(tag_icon);
+
+    var name;
+    if(id === "bot"){
+        name = "AI";
+    }else if(id === "user"){
+        name = "GUEST";
+    }
+    var node_profileName = document.createTextNode(" "+ name);
+    tag_icon.appendChild(node_profileName);
 
     // messageを追加
-    var node_message = document.createTextNode("こんにちは。");
-    bot_profile[bot_profile.length-1].appendChild(node_message);
+    var tag_p = document.createElement("p");
+    tag_p = profile[profile.length-1].appendChild(tag_p);
+    var node_message = document.createTextNode(message);
+    tag_p.appendChild(node_message);
 
-    // 投稿時間の追加
+    // 投稿時間の追加   
     var tag_div = document.createElement("div");
     tag_div.classList.add("time");
     
-    // 投稿時間処理
     var now = new Date();
-    var ampm;
-    if(now.getHours() < 12){
-        ampm = "午前";
-    }else{
-        ampm = "午後";
-    }
-    var time = ampm + now.getHours() % 12 + "時" + now.getMinutes();
+    var time = ampm(now.getHours()) + now.getHours() % 12 + "時" + now.getMinutes() + "分";
     var node_time = document.createTextNode(time);
     tag_div.appendChild(node_time);
-    bot_profile[bot_profile.length-1].appendChild(tag_div);
+    profile[profile.length-1].appendChild(tag_div);
+}
 
+$(document).ready(function(){
 
-    var sendUrl = "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk";
     var apiKey = "";
-    var recieveChat = function(query){
-            $.ajax({
-            url: sendUrl,
+    var host = "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/registration?APIKEY=" + apiKey;
+    var requestbody = {   
+        botId: "Chatting",
+        appKind: "web chat service"
+    };
+    var appId;
+
+    // user登録
+    $.ajax({
+        url: host,
+        type: "POST",
+        contentType: "application/json;charset=UTF-8",
+        dataType: "json",
+        data: JSON.stringify(requestbody)
+    })
+    .done(function(data){
+        appId = data.appId;
+    })
+    .fail(function(){
+        window.alert("error: registration")
+    });
+
+    addChatList("bot", "こんにちは");
+
+    var serverSendTime;
+    var recieveChat = function(message){
+        // recieveTimeの設定
+        var recvTime;
+        if(!serverSendTime){
+            recvTime = "0000-00-00 00:00:00";
+        }else{
+            recvTime = serverSendTime;
+        }
+
+        // sendTimeの設定
+        var now = moment().format().split("T");
+        var ymd = now[0];
+        var hms = now[1].split("+")[0];
+        sendTime = ymd + " " + hms;
+
+        host = "https://api.apigw.smt.docomo.ne.jp/naturalChatting/v1/dialogue?APIKEY=" + apiKey;
+        var setting = {
+            nickname: "GUEST",
+            nicknameY: "ゲスト",
+            sex: "男",
+            // bloodtype: "A",
+            // birthdateY: "1889",
+            // birthdateM: "12",
+            // birthdateD: "12",
+            // age: "25",
+            // constellations:"天秤座",
+            place: "東京",
+            mode: "dialog",
+            t: "kansai"
+        }
+
+        var option = {
+            "option": setting
+        };
+
+        requestbody = {   
+            language: "ja-JP",
+            botId: "Chatting",
+            appId: appId,
+            voiceText: message,
+            clientData: option,
+            appRecvTime: recvTime,
+            appSendTime: sendTime
+        };
+        $.ajax({
+            url: host,
             type: "POST",
-            dataType: "json",
-            data : {
-                "apikey": apiKey,
-                "query": query
-            },
+            contentType: "application/json",
+            data: JSON.stringify(requestbody)
         })
         .done(function(data){
-            // bot側のメッセージをhtmlに表示
-            var bot_message;
-            if(data.message === "ok"){
-                bot_message = data.results[0].reply;
-            }else {
-                bot_message = "もう一度お願いします。"
-            }
+            serverSendTime = data.serverSendTime;
 
-            // liをchat_listに追加
-            var list = document.createElement("li");
-            var tag_list = document.getElementById("chat_list").appendChild(list);
-            // liに.botを追加
-            tag_list.classList.add("bot");
+            var bot_message = data.systemText.expression;
 
-            // アイコンフォントの追加
-            var tag_icon = document.createElement("i");
-            tag_icon.classList.add("fas", "fa-user-circle");
-            var bot_profile = document.getElementsByClassName("bot");
-            bot_profile[bot_profile.length-1].appendChild(tag_icon);
+            addChatList("bot", bot_message);
 
-            // messageを追加
-            var node_message = document.createTextNode(bot_message);
-            bot_profile[bot_profile.length-1].appendChild(node_message);
-
-            // 投稿時間の追加
-            var tag_div = document.createElement("div");
-            tag_div.classList.add("time");
-            
-            // 投稿時間処理
-            var now = new Date();
-            var ampm;
-            if(now.getHours() < 12){
-                ampm = "午前";
-            }else{
-                ampm = "午後";
-            }
-            var time = ampm + now.getHours() % 12 + "時" + now.getMinutes();
-            var node_time = document.createTextNode(time);
-            tag_div.appendChild(node_time);
-            bot_profile[bot_profile.length-1].appendChild(tag_div);
-
-            // 最後尾に移動
             location.href = "#endPoint";
 
-            // テキスト入力フォームをアクティブ
             document.getElementById("form").word.focus();
         })
-        .fail(function(){
-            window.alert("error");
+        .fail(function(data){
+            window.alert("error: dialogue");
+            console.log(data);
         });
     }
 
@@ -112,40 +156,12 @@ $(document).ready(function(){
             return false;
         }
 
-        var list = document.createElement("li");
-        var tag_list = document.getElementById("chat_list").appendChild(list);
-        tag_list.classList.add("user");
+        addChatList("user", user_message);
 
-        var user_profile = document.getElementsByClassName("user");
-
-        // messageを追加
-        var node_message = document.createTextNode(user_message);
-        user_profile[user_profile.length-1].appendChild(node_message);
-
-        // 投稿時間の追加
-        var tag_div = document.createElement("div");
-        tag_div.classList.add("time");
-        
-        // 投稿時間処理
-        var now = new Date();
-        var ampm;
-        if(now.getHours() < 12){
-            ampm = "午前";
-        }else{
-            ampm = "午後";
-        }
-        var time = ampm + now.getHours() % 12 + "時" + now.getMinutes();
-        var node_time = document.createTextNode(time);
-        tag_div.appendChild(node_time);
-        user_profile[user_profile.length-1].appendChild(tag_div);
-
-        // 最後尾に移動
         location.href = "#endPoint";
 
-        // TalkAPI
         recieveChat(user_message);
 
-        // input textのvalueを消去
         document.getElementById("form").word.value = "";
 
         // 再更新を停止
